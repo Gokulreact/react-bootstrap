bf5c2a271b4845a3b189c74cf7c0cf2b3da16859
Gokulreact <gokuljilla555@gmail.com>
2021-03-29 18:51:08 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {uuid} from 'uuidv4';
 import Title from './Pillars/Header/Title';
 import AccordionData from './Pillars/ShowData/AccordionData';
 import AddData from './Pillars/CRUD/AddData';
+import AddSubData from './Pillars/CRUD/AddSubCategory'
 
 function App() {
 
@@ -11,10 +12,38 @@ function App() {
 
   const [contacts, setContacts] = useState([]);
 
+
+
   const addCategoryHandler = (category) =>{
-    //console.log(category);
-    setContacts([...contacts, {id: uuid(), ...category}])
-  ;}
+    console.log(category);
+    //setContacts([...contacts,  category: {id: uuid(), name: category, subcategory: {id:uuid(), link: "gokul.com", name:"SubCat1"}}]);
+   setContacts([...contacts, {
+      Lists: [{
+        id:uuid(),
+        name:category.category,
+        subcategory:{
+          id:uuid(),
+          link:"Gokul123",
+          name:"subcat1"
+        }
+      }]
+    }])
+    
+   /*  setContacts([...contacts, {
+      Lists:[{
+        id:"1",
+       name:"Gkyl1"}]}]);
+  }*/
+  //console.log(...contacts)
+  
+  }
+  const addSubCatHandler = (subcategory) =>{
+  // console.log(subcategory);
+    
+//console.log("Hole data", contacts)
+  //  console.log(subcategory.category)
+    setContacts([...contacts, {id: uuid(),  category: subcategory.category,  subcategory: {...subcategory}}]);
+  }
  
   useEffect(()=>{
     const getData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
@@ -30,9 +59,10 @@ function App() {
   return (
     <div className="App">
        <Title />
-     <AccordionData contacts={contacts}/>
+       <AccordionData contacts={contacts}/>
        <br/>
        <AddData addCategoryHandler = {addCategoryHandler}/>
+       <AddSubData addSubCatHandler = {addSubCatHandler}/>
     </div>
   );
 }
---
+++ src/Pillars/CRUD/AddSubCategory.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+class AddSubCat extends Component {
+
+    constructor(){
+        super();
+         this.state = {
+          category:"Gokul",
+          subcategory: " ",
+          link:""
+        };
+    
+      }
+
+
+    addSub =(e) =>{
+        e.preventDefault();
+       if(this.state.subcategory === "" || this.state.link ===""){
+         alert ("Please enter Fileds");
+         return;
+       } 
+       //console.log(this.state)
+       this.props.addSubCatHandler(this.state);
+       this.setState({subcategory:"", link:""});
+       //alert("helo")
+     }
+
+
+  render() {
+    return (
+      <div className="addBox">
+        <Container>
+          <Form onSubmit={this.addSub}>
+            <Form.Group controlId="formSubCategoryName" as={Row}>
+              <Form.Label column sm="3">
+                Subcategory Name
+              </Form.Label>
+              <Col sm="7">
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Sub Category Name"
+                  value={this.state.subcategory}
+                  onChange={(e) => {
+                    this.setState({ subcategory: e.target.value });
+                  }}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group controlId="formlink" as={Row}>
+              <Form.Label column sm="3">
+                Link URL
+              </Form.Label>
+              <Col sm="7">
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Link URL"
+                  value={this.state.link}
+                  onChange={(e) => {
+                    this.setState({ link: e.target.value });
+                  }}
+                />
+              </Col>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default AddSubCat;
