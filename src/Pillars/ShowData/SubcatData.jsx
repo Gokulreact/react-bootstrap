09bb8a876f28c865979c39e14e684c5bd7201bf6
Gokulreact <gokuljilla555@gmail.com>
2021-04-12 17:32:45 +0530

    Commit

--- src/Pillars/ShowData/SubcatData.jsx
+++ src/Pillars/ShowData/SubcatData.jsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React,{Component} from 'react';
+//import api from "../../api/jsonconnect";
 
-const SubCatDatas = (props) => {
-    console.log(props)
-     
-    const subData = (props) =>{
-        console.log("I a ")
-        return(
-            <h2>Subdata1</h2>
+class SubCatDatas extends Component{
+  constructor(props) {
+      super(props);
+      this.state = { 
+        data: [],
+        categoryName: props.catName };
+  }
+  
 
-        )
-    }
-    subData()
 
-    return(
-        <div>
-        {subData}
-        <h2>Subcat</h2>
-        </div>
-    )
+  componentDidMount() {
+      fetch(`http://localhost:3004/subdatas`)
+      .then(res => res.json())
+      .then(json => this.setState({data: json}));
+       //console.log(this.state.data)
+      
+  }
+
+
+  render(){
+      return(
+          <div>
+          <h1>Subddatas</h1>
+         
+         
+          {this.state.data.map(el => {
+              console.log(el);
+             
+                 
+                  
+                  if((el.category) === (this.state.categoryName)){
+                      return(
+                        <h1>{el.link}</h1>
+                      )
+                      
+
+                  }
+                  
+             
+
+          }
+          )}
+         
+         
+          </div>
+      )
+  }
 }
+    
+    
+
 
     
 
